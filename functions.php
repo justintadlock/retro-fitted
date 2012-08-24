f1dbba1e0bd5ead9692397063355d9fc946e496b
Justin Tadlock <justin@justintadlock.com>
2012-08-24 14:54:29 -0500

    Version bump - 0.3 beta.

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@
  *
  * @package Retro-fitted
  * @subpackage Functions
- * @version 0.2.0
+ * @version 0.3.0
  * @author Justin Tadlock <justin@justintadlock.com>
  * @copyright Copyright (c) 2011, Justin Tadlock
  * @link http://themehybrid.com/themes/retro-fitted
